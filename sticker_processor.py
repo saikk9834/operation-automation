1ca2968d93f107625f847287bb2e733151b1ae14
perceptronq <hungrydude283@gmail.com>
2025-02-12 17:14:33 +0530

    refactor: increase bleeding to 12

--- sticker_processor.py
+++ sticker_processor.py
@@ -17,10 +17,10 @@ class StickerOrder:
     design_url: str
 
 class StickerProcessor:
-    def __init__(self, 
+    def __init__(self,
                  sticker_size: Tuple[int, int] = (2, 2),  # inches
-                 bleeding_pixels: int = 8,
-                 canvas_sizes: List[Tuple[int, int]] = [(10, 8), (10, 10)]):  # inches
+                 bleeding_pixels: int = 12,
+                 canvas_sizes: List[Tuple[int, int]] = [(10, 8), (10, 10)]):
         self.dpi = 300  # 300 pixels per inch
         self.mm_to_pixels = self.dpi / 25.4  # Convert mm to pixels
         
