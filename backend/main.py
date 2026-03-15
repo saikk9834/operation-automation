df7e7325b3747756bc13a2a0066f4b589afeb04b
Saikrishnan Iyer <saikk9834@gmail.com>
2026-03-15 09:59:57 -0500

    Removed exe and tkinter

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@ import sys
 import os
 import shutil
 import csv
-import json
 import script
 import zipfile
 from datetime import datetime
@@ -31,16 +30,6 @@ def resource_path(relative_path):
 
 load_dotenv(dotenv_path=resource_path("../.env"))
 CONFIG_FILE = os.path.join(os.path.expanduser("~"), ".operation_automation_config.json")
-
-
-def select_all_in_one_folder():
-    folder_selected = filedialog.askdirectory()
-    all_in_one_path.set(folder_selected)
-
-
-def select_destination_folder():
-    folder_selected = filedialog.askdirectory()
-    destination_path.set(folder_selected)
 
 
 def zip_folder(folder_path, zip_path):
@@ -135,38 +124,6 @@ def send_email(shared_link, recipient_email, cc_email=None):
     except smtplib.SMTPException as e:
         print(f"Error: Unable to send email - {str(e)}")
 
-
-def load_settings():
-    if os.path.exists(CONFIG_FILE):
-        try:
-            with open(CONFIG_FILE, "r") as f:
-                config = json.load(f)
-                all_in_one_path.set(config.get("all_in_one_path", ""))
-                destination_path.set(config.get("destination_path", ""))
-                recipient_email_var.set(config.get("recipient_email", ""))
-                cc_email_var.set(config.get("cc_email", ""))
-        except Exception as e:
-            messagebox.showwarning("Warning", f"Failed to load settings: {str(e)}")
-
-
-def save_settings():
-    config = {
-        "all_in_one_path": all_in_one_path.get(),
-        "destination_path": destination_path.get(),
-        "recipient_email": recipient_email_var.get(),
-        "cc_email": cc_email_var.get(),
-    }
-    try:
-        with open(CONFIG_FILE, "w") as f:
-            json.dump(config, f)
-    except Exception as e:
-        messagebox.showwarning("Warning", f"Failed to save settings: {str(e)}")
-
-
-def on_closing():
-    save_settings()
-    root.destroy()
-
 def process_sticker_folders(input_dir: Path, output_dir: Path) -> None:
     processor = StickerProcessor()
     output_dir.mkdir(exist_ok=True)
@@ -259,49 +216,3 @@ def run_script(all_in_one,destination,recipient_email,cc_email):
     folder_id = "1eGk8Tuzl1fmOYR8ZUluwW07fBCi800r2"
     shared_link = upload_to_drive(zip_filepath, folder_id)
     send_email(shared_link, recipient_email, cc_email)
-
-
-'''
-root = tk.Tk()
-root.title("Operation Automation")
-
-all_in_one_path = tk.StringVar()
-destination_path = tk.StringVar()
-recipient_email_var = tk.StringVar()
-cc_email_var = tk.StringVar()
-
-# Load the previous settings
-load_settings()
-
-tk.Label(root, text="All in one folder:").grid(row=0, column=0, padx=10, pady=10)
-tk.Entry(root, textvariable=all_in_one_path, width=50).grid(
-    row=0, column=1, padx=10, pady=10
-)
-tk.Button(root, text="Browse", command=select_all_in_one_folder).grid(
-    row=0, column=2, padx=10, pady=10
-)
-
-tk.Label(root, text="Destination folder:").grid(row=1, column=0, padx=10, pady=10)
-tk.Entry(root, textvariable=destination_path, width=50).grid(
-    row=1, column=1, padx=10, pady=10
-)
-tk.Button(root, text="Browse", command=select_destination_folder).grid(
-    row=1, column=2, padx=10, pady=10
-)
-
-tk.Label(root, text="Recipient Email:").grid(row=2, column=0, padx=10, pady=10)
-tk.Entry(root, textvariable=recipient_email_var, width=50).grid(
-    row=2, column=1, padx=10, pady=10
-)
-
-tk.Label(root, text="CC Email:").grid(row=3, column=0, padx=10, pady=5)
-tk.Entry(root, textvariable=cc_email_var, width=50).grid(
-    row=3, column=1, padx=10, pady=5
-)
-
-tk.Button(root, text="Run", command=run_script).grid(row=4, column=1, padx=10, pady=10)
-
-root.protocol("WM_DELETE_WINDOW", on_closing)
-
-root.mainloop()
-'''
